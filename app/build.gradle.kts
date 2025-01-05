9846337973d21c9d1287f8c54e50a77905e7fe64
syphyr <syphyr@gmail.com>
2025-01-05 21:40:27 +0100

    Reduce size of apk by only building arm64 support
    Add abiFilters. Support for every abi is not required for
    testing builds. This also drastically speeds up the build
    times.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,10 @@ android {
         minSdk = 29
         targetSdk = 35
 
+        ndk {
+            abiFilters += listOf("arm64-v8a")
+        }
+
         vectorDrawables.useSupportLibrary = true
 
         androidResources.localeFilters.addAll(getListOfSupportedLocales())

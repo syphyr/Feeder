a43a4fda676e819fe809f00678d12c20ba4395f9
syphyr <syphyr@gmail.com>
2025-04-11 21:09:45 +0200

    Disable Play Store flavor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -124,12 +124,12 @@ android {
                 dimension = "store"
                 // Keeping default version values for F-Droid
             }
-            create("play") {
-                dimension = "store"
-                versionName = "2.12.2"
-                versionCode = commitCount
-                applicationIdSuffix = ".play"
-            }
+//            create("play") {
+//                dimension = "store"
+//                versionName = "2.12.2"
+//                versionCode = commitCount
+//                applicationIdSuffix = ".play"
+//            }
         }
     }
     testOptions {
